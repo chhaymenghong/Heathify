c87a2476d7128c5422409d22aa75520c7232d6c9
chhaymenghong <chhaymenghong@yahoo.com>
2017-11-14 21:18:20 -0800

    Add test route

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Welcome to Healthify'));
+app.post('/intro', function(req, res) {
+    res.send('Drink water now');
+};
 
 // since we are using Heroku to host this server, we need to accept the port number given by Heroku
 const PORT = process.env.PORT || 5000;

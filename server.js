5744b25ad64971c39f6529bb7c5e49de9c32df26
chhaymenghong <chhaymenghong@yahoo.com>
2017-11-14 22:48:26 -0800

    sdf

--- server.js
+++ server.js
@@ -16,17 +16,23 @@ app.all('/', function(req, res, next) {
     next();
 });
 
-app.get('/', (req, res) => res.send('Welcome to Healthify'));
-
 // TODO:HONG use slack awesome feature to style the message
+// TODO:HONG also include a list of command that we have
 app.post('/intro', (req, res) => {
     res.send('Hello Hong. Healthify will remind you when you should drink water!');
+} );
+app.post('/fulltank', (req, res) => {
+    if ( interval ) {
+        clearInterval(interval);
+    }
 } );
 
 app.post('/water', (req, res) => {
     res.send('Drink some water!!');
     // console.log(req.headers['content-type'])
 } );
+
+
 
 // since we are using Heroku to host this server, we need to accept the port number given by Heroku
 const PORT = process.env.PORT || 5000;
@@ -36,7 +42,7 @@ app.listen(PORT, () => console.log('Example app listening on port 5000!'));
 /** Send data from server to slack anytime we want without any request from slack.
  * We can use this to periodically notify user **/
 const oneHour = 3600000;
-setInterval(sendReminder, 5000);
+const interval = setInterval(sendReminder, 5000);
 
 
 function sendReminder() {
@@ -45,7 +51,15 @@ function sendReminder() {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({text: "Hello"})
+
+        body: JSON.stringify({
+            "text": "I am a test message http://slack.com",
+            "attachments": [
+                {
+                    "text": "And here’s an attachment!"
+                }
+            ]
+        })
     };
 
     request.post('https://hooks.slack.com/services/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', options).on('error', function(e) {
@@ -57,8 +71,10 @@ function sendReminder() {
 
 // TODO:HONG for now let have the following feature
     // 1. add a couple of command:
-        // "/intro" give description of what the app is
-        // "/
+        // "/intro" give description of what the app is: also show what kind of command we have
+        // "/waterify when user press this command, give them frequency options.
+        // once you click sth, send that response to the server and start the interval
+        // "/stop" stop the interval
 
 
 

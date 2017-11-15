6386e058d0cf8bcb297263f0dec071d7a1285771
chhaymenghong <chhaymenghong@yahoo.com>
2017-11-14 21:24:02 -0800

    sdf

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Welcome to Healthify'));
-app.post('/intro', function(req, res) {
-    res.send('Drink water now');
-});
+app.post('/water', (req, res) => res.send('Drink some water!!') );
 
 // since we are using Heroku to host this server, we need to accept the port number given by Heroku
 const PORT = process.env.PORT || 5000;
